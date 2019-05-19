25cebfebfed55929f47e635f893aad40e3dc1d92
Todd Watson <todd@showit.co>
2019-05-18 19:16:02 -0700

    Added DB Query for team list

--- includes/Endpoint/Teammate.php
+++ includes/Endpoint/Teammate.php
@@ -89,10 +89,47 @@ class Teammate {
      * @return WP_Error|WP_REST_Request
      */
     public function get_teammate_list( $request ) {
-        //Currently reading sample data from a file and returning
-        $sample = json_decode(file_get_contents(__DIR__.'/sample-data/teammate-list.json'));
+        global $wpdb;
 
-        return new \WP_REST_Response( $sample, 200 );
+        $results = $wpdb->get_results( "SELECT rm.user_id, fname, lname, year, month, total, submitted, reviewed 
+        from {$wpdb->prefix}rhythmus_member rm left outer join {$wpdb->prefix}rhythmus_kra_review rkr on rm.user_id = rkr.user_id 
+        where rm.is_active = 1 order by fname asc, year desc, month desc", OBJECT );
+
+        $teammates = array ();
+        $currUser = false;
+        $currUserID = null;
+        $currScores = array();
+        foreach ( $results as $row ) 
+        {
+            if($row->user_id != $currUserID) {
+                if($currUser) {
+                    $currUser["months"] = $currScores;
+                    array_push($teammates, $currUser);
+                }
+                $currUser = array(
+                    "name" => $row->fname." ".$row->lname,
+                    "userid" => $row->user_id
+                );
+                $currUserID = $row->user_id;
+                $currScores = array();
+            }
+            $key = $row->year."-".$row->month;
+            $currScores[$key] = array(
+                "score" => $row->total,
+                "reviewed" => ($row->reviewed == 1),
+                "submitted" => ($row->submitted == 1),
+            );
+        }
+        if($currUser) {
+            $currUser["months"] = $currScores;
+            array_push($teammates, $currUser);
+        }
+        
+        $teamlist = array(
+            "app" => 'Rhythmus',
+            "version" => 1,
+            "teammates" => $teammates);
+        return new \WP_REST_Response( $teamlist, 200 );
     }
     /**
      * Check if a given request has access
