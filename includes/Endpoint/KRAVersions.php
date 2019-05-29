a6aed952b2e4148542b5b416ff7f3f2382d5f2e4
Kylen Downs <kylen@showit.co>
2019-05-29 15:32:47 -0700

    KRA version file created with todo outline

---
+++ includes/Endpoint/KRAVersions.php
@@ -0,0 +1,11 @@
+<?php
+
+
+/**
+ * - Create an "Add new" button
+ * - Button duplictaes the current KRA data into a new dataset
+ * - Display employee name and version # 
+ * - Button for previous versions
+ *      - Sub menu with previous verions
+ * 
+ */
