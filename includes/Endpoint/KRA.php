3a77adac7fc96c49410790063464f8af263a768d
Ahmed Ghorab <ahmed@showit.co>
2019-06-06 15:40:52 -0700

    /**          * 1. Get data.          * 2. Organize data.          * 3. Display data.          */

--- includes/Endpoint/KRA.php
+++ includes/Endpoint/KRA.php
@@ -12,7 +12,7 @@ use WP_REST_Response;
 /**
  * @subpackage REST_Controller
  */
-class KRAVersions {
+class KRA {
 
     /**
      * Handle endpoint authentication
@@ -76,7 +76,7 @@ class KRAVersions {
         register_rest_route( $namespace, $endpoint, array(
             array(
                 'methods'               => \WP_REST_Server::READABLE,
-                'callback'              => array( $this, 'get_kra_versions' ),
+                'callback'              => array( $this, 'get_kra' ),
                 'permission_callback'   => array( $this->auth, 'permissions_check' ),
                 'args'                  => array(),
             ),
@@ -89,9 +89,22 @@ class KRAVersions {
      * @param WP_REST_Request $request Full data about the request.
      * @return WP_Error|WP_REST_Request
      */
-    public function get_kra_versions( $request ) {
+    public function get_kra( $request ) {
+        /**
+         * 1. Get data.
+         * 2. Organize data.
+         * 3. Display data.
+         */
+
+         /**
+          * 1. Get a KRA record from the database (SQL)
+          * - look at get_kra_topics for example
+          * 2. try and make record look like kra.json sample file
+          * 3. Send it to the display.
+          */
+
         //Currently reading sample data from a file and returning
-        $sample = json_decode(file_get_contents(__DIR__.'/sample-data/wr-status-list.json'));        
+        $sample = json_decode(file_get_contents(__DIR__.'/sample-data/kra.json'));        
         return new \WP_REST_Response( $sample, 200 );
 
     }
