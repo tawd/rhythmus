d3575ff1a97b7f39ed275b414cfcf7b78a96a929
Edwin Smith <thesojourn@gmail.com>
2019-07-03 13:47:39 -0700

    weekly report read summary

--- includes/endpoints/class-weekly-report.php
+++ includes/endpoints/class-weekly-report.php
@@ -16,6 +16,7 @@ use Rhythmus;
 use WP_Error;
 use WP_REST_Request;
 use WP_REST_Response;
+use WP_REST_Server;
 
 /**
  * @subpackage REST_Controller
@@ -31,16 +32,16 @@ class Weekly_Report extends Abstract_Endpoint {
 
 		$this->register_route( $route, array(
 			array(
-				'methods'               => \WP_REST_Server::READABLE,
-				'callback'              => array( $this, 'get_wr_status_list' ),
+				'methods'               => WP_REST_Server::READABLE,
+				'callback'              => array( $this, 'get_status_summary' ),
 				'args'                  => array(),
 			),
 		) );
 
 		$this->register_route( $route, array(
 			array(
-				'methods'               => \WP_REST_Server::EDITABLE,
-				'callback'              => array( $this, 'update_wr_status' ),
+				'methods'               => WP_REST_Server::EDITABLE,
+				'callback'              => array( $this, 'update' ),
 				'permission_callback'   => array( $this->auth, 'permissions_check' ),
 				/**
 				 * These args are the params submitted to this endpoint.
@@ -75,13 +76,56 @@ class Weekly_Report extends Abstract_Endpoint {
 	 * Get Weekly Report Status List
 	 *
 	 * @param WP_REST_Request $request Full data about the request.
-	 * @return WP_Error|WP_REST_Request
+	 * @return WP_REST_Response
 	 */
-	public function get_wr_status_list( $request ) {
-		//Currently reading sample data from a file and returning
-		$sample = json_decode(file_get_contents(__DIR__.'/sample-data/wr-status-list.json'));
-		return new \WP_REST_Response( $sample, 200 );
+	public function get_status_summary( $request ) {
 
+		global $wpdb;
+
+		$results = $wpdb->get_results( "SELECT w.id as week_id, w.start_date, w.end_date, w.num_submitted,
+		w.num_reviewed, r.id as report_id, r.status, r.reviewed, r.submit_date,
+		t.id as teammate_id, CONCAT(t.fname, ' ', t.lname) as name
+		FROM {$wpdb->prefix}rhythmus_weekly_report r
+		LEFT OUTER JOIN {$wpdb->prefix}rhythmus_weekly_report_week w ON w.id = r.week_id
+		JOIN {$wpdb->prefix}rhythmus_teammate t ON r.teammate_id = t.id;");
+
+		$weeks = array();
+		$teammates = array();
+
+		foreach ( $results as $result ) {
+
+			if ( ! array_key_exists( $result->week_id, $weeks ) ) {
+				$weeks[ $result->week_id ] = array(
+					'id' => $result->week_id,
+					'start_date' => $result->start_date,
+					'end_date' => $result->end_date,
+					'num_submitted' => $result->num_submitted,
+					'num_reviewed' => $result->num_reviewed,
+				);
+			}
+
+			if ( ! array_key_exists( $result->name, $teammates ) ) {
+				$teammates[ $result->name ] = array(
+					'name' => $result->name,
+					'user_id' => $result->teammate_id,
+					'weeks' => array(),
+				);
+			}
+
+			$teammates[ $result->name ]['weeks'][ $result->week_id ] = array(
+				'id' => $result->report_id,
+				'status' => $result->status,
+				'reviewed' => (int) $result->reviewed === 1,
+				'submitted' => empty( $result->submit_date ) ? FALSE : TRUE,
+			);
+		}
+
+		$response_data = array(
+			'weeks' => array_values( $weeks ),
+			'teammates' => array_values( $teammates ),
+		);
+
+		return $this->endpoint_response( $response_data );
 	}
 
 	/**
@@ -114,27 +158,12 @@ class Weekly_Report extends Abstract_Endpoint {
 	}
 
 	/**
-	 * Validate any values that ought to be integers.
-	 *
-	 * @param mixed $value
-	 * @param WP_REST_Request $request
-	 * @param string $param
-	 * @return WP_Error
-	 */
-	public function validate_int( $value, $request, $param ) {
-
-		if ( ! is_int( (int) $value ) ) {
-			return new WP_Error( 'rest_invalid_param', 'This value needs to be a number.' );
-		}
-	}
-
-	/**
 	 * Create OR Update
 	 *
 	 * @param WP_REST_Request $request Full data about the request.
 	 * @return WP_REST_Response
 	 */
-	public function update_wr_status( $request ) {
+	public function update( $request ) {
 		global $wpdb;
 
 		/**
