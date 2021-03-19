3c1878a3f9b42ddc43487e1367597a66e98bee41
Edwin Smith <edwin@showit.co>
2021-03-19 12:55:27 -0700

    linters

---
+++ app/.eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+    env: {
+      browser: true,
+      node: true,
+    },
+    extends: [
+      "airbnb",
+      "airbnb/hooks",
+      "prettier",
+      "prettier/prettier",
+      "plugin:prettier/recommended",
+      "prettier/react",
+    ],
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 12,
+      sourceType: "module",
+    },
+    plugins: ["react"],
+    rules: {
+      "prefer-const": [1],
+      "no-unused-vars": [1],
+      "react/prop-types": [1],
+      "react/destructuring-assignment": [1],
+      "react/jsx-filename-extension": [
+        1,
+        {
+          extensions: [".js", ".jsx"],
+        },
+      ],
+    },
+  };
+  
