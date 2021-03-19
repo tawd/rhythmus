6930677ef1fe047ff2eff253fa3a1e30007644c5
Edwin Smith <edwin@showit.co>
2021-03-19 12:55:43 -0700

    formatting

--- includes/endpoints/class-milestonia.php
+++ includes/endpoints/class-milestonia.php
@@ -2,19 +2,12 @@
 /**
  * Rhythmus
  *
- *
  * @package   Rhythmus
  * @author    Josh Ahles
  * @license   GPL-3.0
  * @link      https://showit.co
  * @copyright 2019 Showit, Inc
  */
-
-
-
-//TASKS FOR NEXT WEDNESDAY
-// - Clean up code - no printing just returning
-// - Authenticate requests 
 
 namespace Rhythmus\Endpoints;
 
@@ -35,13 +28,16 @@ class Milestonia extends Abstract_Endpoint {
 
 		$milestonia_route = '/milestonia';
 
-		$this->register_route( $milestonia_route, array(
+		$this->register_route(
+			$milestonia_route,
 			array(
-				'methods'  => \WP_REST_Server::READABLE,
-				'callback' => array( $this, 'get_milestonia' ),
-				'args'     => array(),
-			),
-		) );
+				array(
+					'methods'  => \WP_REST_Server::READABLE,
+					'callback' => array( $this, 'get_milestonia' ),
+					'args'     => array(),
+				),
+			)
+		);
 	}
 
 	/**
@@ -52,38 +48,38 @@ class Milestonia extends Abstract_Endpoint {
 	 * @return WP_REST_Response
 	 */
 	public function get_milestonia( $request ) {
-		
+
 		$teammate_id = $request->get_param( 'teammate_id' );
-		$users_email = $this->get_user_email($teammate_id);
-		$api_token = $this->get_token();
-		$api = $this->get_api($users_email, $api_token);
+		$users_email = $this->get_user_email( $teammate_id );
+		$api_token   = $this->get_token();
+		$api         = $this->get_api( $users_email, $api_token );
 
-		//get the api data 
-		$api_data = $this->get_data($api);
+		// get the api data
+		$api_data = $this->get_data( $api );
 
-		//convert the api data
-		$conversion = $this->convert($api_data);
+		// convert the api data
+		$conversion = $this->convert( $api_data );
 
-		//////////////////
-		//validate token
-		//////////////////
+		//
+		// validate token
+		//
 		$wp_id = $this->auth->permissions_check( $request );
-		if(!$wp_id){
+		if ( ! $wp_id ) {
 			return $this->endpoint_response(
-				new WP_Error( 'Rhythmus_milestonia', 'invalid user ')
+				new WP_Error( 'Rhythmus_milestonia', 'invalid user ' )
 			);
 		}
 
-		//lookup teammate
+		// lookup teammate
 		$email = true;
-		if($teammate_id){
+		if ( $teammate_id ) {
 
-		}else{
+		} else {
 
 		}
-		if(!$email){
+		if ( ! $email ) {
 			return $this->endpoint_response(
-				new WP_Error( 'Rhythmus_milestonia', 'unable to find email ')
+				new WP_Error( 'Rhythmus_milestonia', 'unable to find email ' )
 			);
 		}
 
@@ -94,59 +90,62 @@ class Milestonia extends Abstract_Endpoint {
 	public function get_user_email( $teammate_id ) {
 		global $wpdb;
 
-		$query = $wpdb->prepare("SELECT u.user_email,t.fname,t.lname
+		$query = $wpdb->prepare(
+			"SELECT u.user_email,t.fname,t.lname
 		FROM wp_users u 
 		JOIN {$wpdb->prefix}rhythmus_teammate t
 		ON t.wp_user_id = u.ID
-		WHERE t.id = %d", $teammate_id);
+		WHERE t.id = %d",
+			$teammate_id
+		);
 
 		$user = $wpdb->get_row( $query );
 
 		if ( ! $user ) {
 			return '';
 		}
-		
-		return $user->user_email;		
+
+		return $user->user_email;
 	}
 
-	public function get_api ($users_email, $token) {
+	public function get_api( $users_email, $token ) {
 
 		$url = "https://app.milestonia.com/api/v1/goals?email={$users_email}&api_token={$token}";
 		return $url;
 
 	}
 
-	public function wp_remote_get($url, $args = array()) {
+	public function wp_remote_get( $url, $args = array() ) {
 		$http = _wp_http_get_object();
-		return $http-> get( $url, $args);
+		return $http->get( $url, $args );
 	}
 
-	//get token method
-	public function get_token () {
+	// get token method
+	public function get_token() {
 
-		return get_option('milestonia_api_key', '');
-	
+		return get_option( 'milestonia_api_key', '' );
+
 	}
 
-	public function get_json_string_to_arry ($api) {
+	public function get_json_string_to_arry( $api ) {
 
-		$response = file_get_contents($api, false);
-		$data = json_decode($response);
-		
+		$response = file_get_contents( $api, false );
+		$data     = json_decode( $response );
+
 		return $data;
 
 	}
-	
-	public function get_data($api) {
 
-		$remote_get = wp_remote_get( esc_url_raw($api));
+	public function get_data( $api ) {
+
+		$remote_get = wp_remote_get( esc_url_raw( $api ) );
 		return $remote_get;
 
 	}
 
-	public function convert($remote_get) {
+	public function convert( $remote_get ) {
 
-		$conversion = json_decode(wp_remote_retrieve_body($remote_get), true);
+		$conversion = json_decode( wp_remote_retrieve_body( $remote_get ), true );
 		return $conversion;
 
 	}
